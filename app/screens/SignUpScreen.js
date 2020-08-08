57547d2dd070f0fb9916b9f398b32895a7b4c0ac
jchelton <jchelton@ncsu.edu>
2020-08-08 19:11:21 -0400

    SignUp page UI tweaks and Add Picker

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {Picker} from '@react-native-community/picker';
 import auth from '@react-native-firebase/auth';
 import logo from '../assets/logo.png';
 import {summitBlue} from '../assets/colors';
@@ -18,21 +19,23 @@ export default function SignUpScreen({navigation}) {
   const [last, setLast] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [school, setSchool] = useState('');
+  const [gradYear, setGradYear] = useState('');
 
   const [isReadyToSubmit, setIsReadyToSubmit] = useState(false);
 
   useEffect(() => {
-    if (first && last && email && password) {
+    if (first && last && email && password && school && gradYear) {
       setIsReadyToSubmit(true);
+    } else {
+      setIsReadyToSubmit(false);
     }
-  }, [first, last, email, password]);
+  }, [first, last, email, password, school, gradYear]);
 
   const handleSignUp = () => {
     auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log('User account created & signed in!');
-      })
+      .then(() => console.log('User account created & signed in!'))
       .catch((error) => {
         if (error.code === 'auth/email-already-in-use') {
           Alert.alert('Sorry, this email address is already in use.');
@@ -46,13 +49,15 @@ export default function SignUpScreen({navigation}) {
       });
   };
 
+  // HANDLE ERRORS
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.title}>Summit College</Text>
         <Image source={logo} style={styles.logo} />
+        <Text style={styles.createText}>Create your account</Text>
       </View>
-      <Text style={styles.createText}>Create your account</Text>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="First Name"
@@ -79,25 +84,53 @@ export default function SignUpScreen({navigation}) {
           secureTextEntry
           style={styles.textInput}
         />
+        <View style={styles.pickerContainer}>
+          <Picker
+            style={styles.schoolPicker}
+            selectedValue={school}
+            onValueChange={(itemValue, itemIndex) => setSchool(itemValue)}>
+            <Picker.Item label="School" value="" />
+            <Picker.Item label="NCSU" value="NCSU" />
+            <Picker.Item label="UNC" value="UNC" />
+            <Picker.Item label="NCCU" value="NCCU" />
+            <Picker.Item label="Meredith" value="Meredith" />
+          </Picker>
+          <Picker
+            style={styles.gradPicker}
+            selectedValue={gradYear}
+            onValueChange={(itemValue, itemIndex) => setGradYear(itemValue)}>
+            <Picker.Item label="Graduation Year" value="" />
+            <Picker.Item label="2020" value="2020" />
+            <Picker.Item label="2021" value="2021" />
+            <Picker.Item label="2022" value="2022" />
+            <Picker.Item label="2023" value="2023" />
+            <Picker.Item label="2024" value="2024" />
+            {/* Eventually we should add a function that calculates the next 5 years from the current year */}
+          </Picker>
+        </View>
       </View>
-      {/* Conditionally render a gray button when the text fields aren't entered and a blue button once they are all met */}
-      {!isReadyToSubmit && (
-        <TouchableOpacity style={styles.createAccountButton}>
-          <Text style={styles.createAccountButtonText}>CREATE ACCOUNT</Text>
-        </TouchableOpacity>
-      )}
-      {isReadyToSubmit && (
+      <View style={styles.loginContainer}>
+        {/* Conditionally render a gray button when the text fields aren't entered and a blue button once they are all met */}
+        {!isReadyToSubmit && (
+          <TouchableOpacity style={styles.createAccountButton}>
+            <Text style={styles.createAccountButtonText}>CREATE ACCOUNT</Text>
+          </TouchableOpacity>
+        )}
+        {isReadyToSubmit && (
+          <TouchableOpacity
+            style={styles.createAccountButtonActive}
+            onPress={() => handleSignUp()}>
+            <Text style={styles.createAccountButtonTextActive}>
+              CREATE ACCOUNT
+            </Text>
+          </TouchableOpacity>
+        )}
         <TouchableOpacity
-          style={styles.createAccountButtonActive}
-          onPress={() => handleSignUp()}>
-          <Text style={styles.createAccountButtonTextActive}>
-            CREATE ACCOUNT
-          </Text>
+          style={styles.loginButtonTouchable}
+          onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.loginButton}>LOGIN</Text>
         </TouchableOpacity>
-      )}
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text style={styles.loginButton}>LOGIN</Text>
-      </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 }
@@ -107,24 +140,47 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
   },
   headerContainer: {
-    display: 'flex',
+    marginTop: 50,
+    flex: 1,
   },
   createText: {
     fontSize: 24,
     color: summitBlue,
+    alignSelf: 'center',
+    marginTop: 25,
   },
   inputContainer: {
+    marginTop: 0,
+    display: 'flex',
+    flex: 7,
+    justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  loginContainer: {
+    flex: 2,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  loginContainer: {
+  pickerContainer: {
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  schoolPicker: {
+    flex: 1,
+    height: 0,
+    marginHorizontal: 10,
+  },
+  gradPicker: {
+    flex: 1,
+    height: 0,
+    marginHorizontal: 10,
+  },
+  loginButtonTouchable: {
+    marginTop: 25,
   },
   title: {
     fontSize: 44,
@@ -140,6 +196,8 @@ const styles = StyleSheet.create({
     height: 40,
     borderBottomColor: '#ddd',
     borderBottomWidth: 1,
+    fontSize: 18,
+    marginVertical: 5,
   },
   createAccountButton: {
     backgroundColor: 'gray',
