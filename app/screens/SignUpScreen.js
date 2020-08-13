087355ce97e34da0f535fb5fcd2006a6d2ccac21
jchelton <jchelton@ncsu.edu>
2020-08-13 10:39:17 -0400

    Added KeyboardingAvoidingView and TouchableWithoutFeedback for dismissing

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 import {
   Alert,
   Image,
-  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
+  Keyboard,
 } from 'react-native';
 import {Picker} from '@react-native-community/picker';
 import auth from '@react-native-firebase/auth';
@@ -71,86 +74,94 @@ export default function SignUpScreen({navigation}) {
   // HANDLE ERRORS
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.title}>Summit College</Text>
-        <Image source={logo} style={styles.logo} />
-        <Text style={styles.createText}>Create your account</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="First Name"
-          onChangeText={(val) => setFirst(val)}
-          autoCapitalize="words"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="Last Name"
-          onChangeText={(val) => setLast(val)}
-          autoCapitalize="words"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="Email"
-          onChangeText={(val) => setEmail(val)}
-          autoCapitalize="none"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="Password"
-          onChangeText={(val) => setPassword(val)}
-          autoCapitalize="none"
-          secureTextEntry
-          style={styles.textInput}
-        />
-        <View style={styles.pickerContainer}>
-          <Picker
-            style={styles.schoolPicker}
-            selectedValue={school}
-            onValueChange={(itemValue, itemIndex) => setSchool(itemValue)}>
-            <Picker.Item label="School" value="" />
-            <Picker.Item label="NCSU" value="NCSU" />
-            <Picker.Item label="UNC" value="UNC" />
-            <Picker.Item label="NCCU" value="NCCU" />
-            <Picker.Item label="Meredith" value="Meredith" />
-          </Picker>
-          <Picker
-            style={styles.gradPicker}
-            selectedValue={gradYear}
-            onValueChange={(itemValue, itemIndex) => setGradYear(itemValue)}>
-            <Picker.Item label="Graduation Year" value="" />
-            <Picker.Item label="2020" value="2020" />
-            <Picker.Item label="2021" value="2021" />
-            <Picker.Item label="2022" value="2022" />
-            <Picker.Item label="2023" value="2023" />
-            <Picker.Item label="2024" value="2024" />
-            {/* Eventually we should add a function that calculates the next 5 years from the current year */}
-          </Picker>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.title}>Summit College</Text>
+            <Image source={logo} style={styles.logo} />
+            <Text style={styles.createText}>Create your account</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="First Name"
+              onChangeText={(val) => setFirst(val)}
+              autoCapitalize="words"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="Last Name"
+              onChangeText={(val) => setLast(val)}
+              autoCapitalize="words"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="Email"
+              onChangeText={(val) => setEmail(val)}
+              autoCapitalize="none"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="Password"
+              onChangeText={(val) => setPassword(val)}
+              autoCapitalize="none"
+              secureTextEntry
+              style={styles.textInput}
+            />
+            <View style={styles.pickerContainer}>
+              <Picker
+                style={styles.schoolPicker}
+                selectedValue={school}
+                onValueChange={(itemValue) => setSchool(itemValue)}>
+                <Picker.Item label="School" value="" />
+                <Picker.Item label="NCSU" value="NCSU" />
+                <Picker.Item label="UNC" value="UNC" />
+                <Picker.Item label="NCCU" value="NCCU" />
+                <Picker.Item label="Meredith" value="Meredith" />
+              </Picker>
+              <Picker
+                style={styles.gradPicker}
+                selectedValue={gradYear}
+                onValueChange={(itemValue) => setGradYear(itemValue)}>
+                <Picker.Item label="Graduation Year" value="" />
+                <Picker.Item label="2020" value="2020" />
+                <Picker.Item label="2021" value="2021" />
+                <Picker.Item label="2022" value="2022" />
+                <Picker.Item label="2023" value="2023" />
+                <Picker.Item label="2024" value="2024" />
+                {/* Eventually we should add a function that calculates the next 5 years from the current year */}
+              </Picker>
+            </View>
+          </View>
+          <View style={styles.loginContainer}>
+            {/* Conditionally render a gray button when the text fields aren't entered and a blue button once they are all met */}
+            {!isReadyToSubmit && (
+              <TouchableOpacity style={styles.createAccountButton}>
+                <Text style={styles.createAccountButtonText}>
+                  CREATE ACCOUNT
+                </Text>
+              </TouchableOpacity>
+            )}
+            {isReadyToSubmit && (
+              <TouchableOpacity
+                style={styles.createAccountButtonActive}
+                onPress={() => handleSignUp()}>
+                <Text style={styles.createAccountButtonTextActive}>
+                  CREATE ACCOUNT
+                </Text>
+              </TouchableOpacity>
+            )}
+            <TouchableOpacity
+              style={styles.loginButtonTouchable}
+              onPress={() => navigation.navigate('Login')}>
+              <Text style={styles.loginButton}>LOGIN</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={styles.loginContainer}>
-        {/* Conditionally render a gray button when the text fields aren't entered and a blue button once they are all met */}
-        {!isReadyToSubmit && (
-          <TouchableOpacity style={styles.createAccountButton}>
-            <Text style={styles.createAccountButtonText}>CREATE ACCOUNT</Text>
-          </TouchableOpacity>
-        )}
-        {isReadyToSubmit && (
-          <TouchableOpacity
-            style={styles.createAccountButtonActive}
-            onPress={() => handleSignUp()}>
-            <Text style={styles.createAccountButtonTextActive}>
-              CREATE ACCOUNT
-            </Text>
-          </TouchableOpacity>
-        )}
-        <TouchableOpacity
-          style={styles.loginButtonTouchable}
-          onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.loginButton}>LOGIN</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -159,11 +170,11 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   headerContainer: {
     marginTop: 50,
-    flex: 1,
+    alignItems: 'center',
   },
   createText: {
     fontSize: 24,
@@ -174,29 +185,32 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginTop: 0,
     display: 'flex',
-    flex: 7,
-    justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  loginContainer: {
-    flex: 2,
-    display: 'flex',
+    // flex: 7,
     justifyContent: 'center',
     alignItems: 'center',
   },
   pickerContainer: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
   },
   schoolPicker: {
-    flex: 1,
-    height: 0,
+    width: 150,
+    height: 100,
     marginHorizontal: 10,
+    transform: [{scaleX: 0.8}, {scaleY: 0.8}],
   },
   gradPicker: {
-    flex: 1,
-    height: 0,
+    width: 150,
+    height: 100,
     marginHorizontal: 10,
+    transform: [{scaleX: 0.8}, {scaleY: 0.8}],
+  },
+  loginContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
   },
   loginButtonTouchable: {
     marginTop: 25,
